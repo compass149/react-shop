e9813f9cb38e168dde473e65ee9ba29164f25d75
compass149 <a01055821627@gmail.com>
2024-12-03 15:29:05 +0900

    회원가입 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import NotFound from './pages/not-found/NotFound';
+import UnAuthorized from './pages/unauthorized/UnAuthorized';
+import { Role } from './models/Role';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar />
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/home' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+          <Route path='/404' element={<NotFound />}></Route>
+          <Route path='/401' element={<UnAuthorized />}></Route>
+          <Route path='*' element={<NotFound />}></Route>
+        </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
